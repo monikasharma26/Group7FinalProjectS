c92246c23acf8b9268d699332511be7b0bafdd00
Monika <Monikasharma26081991@gmail.com>
2020-02-19 21:16:35 -0500

    [Monika]  Display Comment Added

--- Group7FinalProjectS/Protocol/IDisplay.swift
+++ Group7FinalProjectS/Protocol/IDisplay.swift
@@ -5,7 +5,7 @@
 //  Created by S@i on 2020-02-19.
 //  Copyright © 2020 S@i. All rights reserved.
 //
-
+//Created Display Protocol
 import Foundation
 protocol IDisplay {
     func display() -> String
