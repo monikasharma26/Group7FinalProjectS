db1a2aca4cd3e61678f8c24c7b562aff6d97edee
Monika <Monikasharma26081991@gmail.com>
2020-02-19 21:17:41 -0500

    [Monika] Person Protocol Added

---
+++ Group7FinalProjectS/Protocol/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Group7FinalProjectS
+//
+//  Created by S@i on 2020-02-19.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+protocol  Person
+{
+    <#requirements#>
+}
