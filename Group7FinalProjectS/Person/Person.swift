eeb21d630356c52406f239399adc9efcaa0da267
Monika <Monikasharma26081991@gmail.com>
2020-02-21 15:04:38 -0500

    [Monika] Created Person protocol

--- Group7FinalProjectS/Person/Person.swift
+++ Group7FinalProjectS/Person/Person.swift
@@ -5,9 +5,14 @@
 //  Created by S@i on 2020-02-19.
 //  Copyright © 2020 S@i. All rights reserved.
 //
+enum Gender
+{
+        case Male
+        case Female
+        case Others
+}
 
 import Foundation
-class Person
-{
-    
+
+    protocol Person : IDisplay{
 }
