b68eeab6ef817c92dba775605e9e89c48b9417ed
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-02-21 16:46:50 -0500

    [KRITIMA] Vehicle rent variables declared

--- Group7FinalProjectS/Rent/VehicleRent.swift
+++ Group7FinalProjectS/Rent/VehicleRent.swift
@@ -7,7 +7,8 @@
 //
 
 import Foundation
-class VehicleRent
+
+class VehicleRent: IDisplay
 {
     var rentStartDate: Date
     var rentEndDate: Date
@@ -15,149 +16,10 @@ class VehicleRent
     var rentedDays: Int
     var vehicleId: String
     var vehicleName: String
+    var vehicleType: VehicleType
     var noOfKmDrived: Float
+    var totalFare: Float
     var customerId: Int
     var customerName: String
     
-    init(cId: Int, rStartDate: Date, rEndDate: Date, rDays: Int, vId: String, nOfKmDrived:Float )
-    {
-        self.customerId = cId
-        self.rentStartDate = rStartDate
-        self.rentEndDate = rEndDate
-        self.rentedDays = rDays
-        self.vehicleId = vId
-        self.noOfKmDrived = nOfKmDrived
-    }
-    
-    func getTotalFare() {
-        switch (vehicleType) {
-            case CAR: {
-                totalFare = 100 * getRentedDays() + (getNoOfKmDrived() * 5);
-                TotalAmount=totalFare;
-                break;
-            }
-            case MOTORCYCLE: {
-                totalFare = 50 * getRentedDays() + (getNoOfKmDrived() * 1);
-                TotalAmount=totalFare;
-                break;
-            }
-            case BUS: {
-                totalFare = 250 * getRentedDays() + (getNoOfKmDrived() * 7);
-                TotalAmount=totalFare;
-                break;
-            }
-            default:
-                totalFare = 0;
-                break;
-        }
-        return totalFare;
-    }
 }
-
-
-  
-
-   /* public VehicleRent(int customerId, LocalDate rentStartDate, LocalDate rentEndDate, VehicleManagement.VEHICLETYPE vehicleType, String vehicleId, float noOfKmDrived) {
-    
-        this.rentEndDate = rentEndDate;
-        this.vehicleType = vehicleType;
-        this.vehicleId = vehicleId;
-        this.noOfKmDrived = noOfKmDrived;
-        getCustomerName();
-    }
-
-    public VehicleRent(int customerId,LocalDate rentStartDate, LocalDate rentEndDate,
-                       VehicleManagement.VEHICLETYPE vehicleType,String vehicleId, float noOfKmDrived,
-                        Vehicle vehicle) {
-        getCustomerName();
-        this.rentStartDate = rentStartDate;
-        this.rentEndDate = rentEndDate;
-        this.vehicleId = vehicleId;
-        this.vehicleType = vehicleType;
-        this.noOfKmDrived = noOfKmDrived;
-        this.customerId = customerId;
-        this.vehicle = vehicle;
-    }
-
-    public String getVehicleName() {
-        if (Vehicle.vehicleList.containsKey(vehicleId)) {
-            vehicleName = Vehicle.vehicleList.get(String.valueOf(vehicleId));
-        } else {
-            vehicleName = Vehicle.vehicleList.get(String.valueOf(vehicleId));
-        }
-        return vehicleName;
-    }
-    public static String getCustomerName(){
-        if (Customer.customerList.containsKey(String.valueOf(customerId))) {
-            customerName = Customer.customerList.get(String.valueOf(customerId));
-        } else {
-            customerName = "Invalid Entry";
-        }
-        return customerName;
-    }
-
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
-
-    public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
-    }
-
-    public VehicleManagement.VEHICLETYPE getVehicleType() {
-        return vehicleType;
-    }
-    public static void setVehicleType(VehicleManagement.VEHICLETYPE vehicleType) {
-        vehicleType = vehicleType;
-    }
-    public static LocalDate getRentStartDate() {
-        return rentStartDate;
-    }
-    public static void setRentStartDate(LocalDate rentStartDate) {
-        rentStartDate = rentStartDate;
-    }
-    public static LocalDate getRentEndDate() {
-        return rentEndDate;
-    }
-    public void setRentEndDate(LocalDate rentEndDate) {
-        this.rentEndDate = rentEndDate;
-    }
-    public static long getRentedDays() {
-        rentedDays = getRentStartDate().until(getRentEndDate(), ChronoUnit.DAYS);
-        return rentedDays;
-    }
-    public static float getNoOfKmDrived() {
-        return noOfKmDrived;
-    }
-    public void setNoOfKmDrived(float noOfKmDrived) {
-        this.noOfKmDrived = noOfKmDrived;
-    }
-    public static float
-/*    public void setTotalFare(float totalFare) {
-        this.totalFare = totalFare;
-    }*/
-public static void TotalAmount(){
-
-}
-    public String display() {
-
-        StringBuilder stringBuilder = new StringBuilder();
-        return String.valueOf(stringBuilder);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Rent Start Date: ").append(getRentStartDate()).append("\n")
-                .append("Rent End Date: ").append(getRentEndDate()).append("\n")
-                .append("Customer Name: ").append(getCustomerName()).append("\n")
-             //   .append("-Vehicle Name: ").append(getVehicleName()).append("\n")
-                .append("Vehicle Type: ").append(getVehicleType()).append("\n")
-                .append("No. of Km. drived: ").append(getNoOfKmDrived() + "km").append("\n")
-                .append("Total bill to pay: $" ).append(getTotalFare());
-        return String.valueOf(stringBuilder);
-    }
-
-
-}
-*/
