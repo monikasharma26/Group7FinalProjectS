087b679e6e22f04fe14dfe0b6e606e0d3ba39ccd
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-02-20 20:55:29 -0500

    [KRITIMA] Extension for speed added

---
+++ Group7FinalProjectS/Main/Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.swift
+//  Group7FinalProjectS
+//
+//  Created by Kritima Kukreja on 2020-02-20.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+func units()-> String{
+           return "\(self) km/hr"
+}
+}
