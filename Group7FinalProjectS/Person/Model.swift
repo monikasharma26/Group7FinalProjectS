ea29b4903f16de55849b5c9faccaf76699d36fbe
Monika <Monikasharma26081991@gmail.com>
2020-02-22 12:31:02 -0500

    [Monika] Model Class Added

---
+++ Group7FinalProjectS/Person/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Group7FinalProjectS
+//
+//  Created by S@i on 2020-02-22.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+public class Model
+{
+   
+}
+
