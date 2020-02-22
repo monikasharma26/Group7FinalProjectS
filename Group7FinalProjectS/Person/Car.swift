95205eabba5aa98bce6b6a044a5c82390c4a697a
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-02-22 12:46:34 -0500

    [KRITIMA] Car class modified

--- Group7FinalProjectS/Person/Car.swift
+++ Group7FinalProjectS/Person/Car.swift
@@ -9,35 +9,13 @@
 import Foundation
 class Car: Vehicle
 {
-    var identificationNumber: String
-    var description: String
-    var isSelfDrive: Bool
-    var manufacturerName: String
-    var isInsured: Bool
-    var vehicleNoOfSeats: Int
-    var insuranceProviderName: String
-    var seat: Int
-    var fuelType: String
-    var ratePerDay: Double
-    var ratePerKm: Double
+    var carType: CarType
     var carColor: String
-    var carType: String
     
     
     init(identificationNumber:String,description:String,isSelfDrive:Bool,
-         manufacturerName: String,isInsured: Bool,vehicleNoOfSeats:Int,insuranceProviderName: String, seat: Int,fuelType: String,ratePerDay: Double,ratePerKm: Double, carColor: String, carType: String)
+         manufacturerName: String,isInsured: Bool,vehicleNoOfSeats:Int,insuranceProviderName: String, seat: Int,fuelType: String,ratePerDay: Double,ratePerKm: Double, carColor: String, carType: CarType)
     {
-        self.identificationNumber = identificationNumber
-        self.description = description
-        self.isSelfDrive = isSelfDrive
-        self.manufacturerName = manufacturerName
-        self.isInsured = isInsured
-        self.vehicleNoOfSeats = vehicleNoOfSeats
-        self.insuranceProviderName = insuranceProviderName
-        self.seat = seat
-        self.fuelType = fuelType
-        self.ratePerDay = ratePerDay
-        self.ratePerKm = ratePerKm
         self.carColor = carColor
         self.carType = carType
     }
