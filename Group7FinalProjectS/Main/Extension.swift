8ca3a35c31835586f5c761179dabb2535f6fd65d
Monika <Monikasharma26081991@gmail.com>
2020-02-23 15:03:26 -0500

    [Monika] String+Extension updated

--- Group7FinalProjectS/Main/Extension.swift
+++ Group7FinalProjectS/Main/Extension.swift
@@ -14,7 +14,7 @@ extension String {
   let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
      let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
      return emailTest.evaluate(with: self)
-}
+        }
 
       func isValidPassword() -> Bool{
              let passwordRegEx = "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}"
