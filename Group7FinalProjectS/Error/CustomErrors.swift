c46ce97d1f3ca515f3506e83ad37dcd931e5b9bf
Monika <Monikasharma26081991@gmail.com>
2020-02-24 19:19:18 -0500

    [Monika] CustomError Added

---
+++ Group7FinalProjectS/Error/CustomErrors.swift
@@ -0,0 +1,14 @@
+//
+//  CustomErrors.swift
+//  Group7FinalProjectS
+//
+//  Created by S@i on 2020-02-24.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+enum CustomerError:Error
+{
+    case emailInvalid, mobileInvalid
+}
+
