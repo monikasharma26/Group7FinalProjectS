3797971466b7a122b76bd06ae73eac0c1ced704e
Monika <Monikasharma26081991@gmail.com>
2020-02-24 19:22:58 -0500

    [Monika] String Extension Added

--- Group7FinalProjectS/Extensions/String+Extension.swift
+++ Group7FinalProjectS/Extensions/String+Extension.swift
@@ -9,4 +9,26 @@
 import Foundation
 extension String{
     
+    public static func formattedDate(sDate : String) -> Date?
+    {
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "dd-MM-YYYY"
+        let formattedDate : Date? = dateFormatterPrint.date(from: sDate)
+        return formattedDate
+    }
+    
+    func addToFile() {
+           let filename = getDocumentsDirectory().appendingPathComponent("output.txt")
+           do {
+               try write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+           } catch {
+               print(error.localizedDescription)
+           }
+       }
+       
+       func getDocumentsDirectory() -> URL {
+           let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+           return paths[0]
+       }
+    
 }
