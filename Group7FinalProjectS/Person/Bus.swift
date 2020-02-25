9bb0f8dd3174ee16d79f7044d2f7260782984a98
Monika <Monikasharma26081991@gmail.com>
2020-02-24 19:32:46 -0500

    [Monika] Bus Class Updated

--- Group7FinalProjectS/Person/Bus.swift
+++ Group7FinalProjectS/Person/Bus.swift
@@ -7,60 +7,84 @@
 //
 
 import Foundation
-class Bus
+class Bus : Vehicle
 {
-   /* var busType: BusType
+    
+    
+    var vehicleIdentificationNumber: String
+    
+    var vehicleDescription: String
+    
+    var manufacturerName: String
+    
+    var isSelfDrive: Bool
+    
+    var isInsured: Bool
+    
+    var noOfSeat: Int
+    
+    var fuelType: String
+    
+    var vehicleType: VehicleT
+    var details: VehicleRentM?
+   var busType: String?
     var isWifiAvailable: Bool
     var isAccessibiltyServiceAvailable: Bool
     
-    init(vIdentificationNumber: String, vDescription: String, mName:String, selfDrive: Bool, insured: Bool, seat: Int, fType: FuelType, insuranceProviderName: String, rPerDay: Double, rPerKm: Double, busType : BusType , wifiAvailable : Bool, accessibiltyServiceAvailable: Bool)
-       {
-           
-                    self.busType = busType
-                     self.isWifiAvailable = wifiAvailable
-                 self.isAccessibiltyServiceAvailable = accessibiltyServiceAvailable
-       }
+    init(vehicleIdentificationNumber: String, vehicleDescription: String, manufacturerName:String, isSelfDrive: Bool, isInsured: Bool, noOfSeat: Int, fuelType: String, vehicleType: VehicleT,
+         isWifiAvailable: Bool, isAccessibiltyServiceAvailable: Bool ){
+        
+          self.vehicleIdentificationNumber = vehicleIdentificationNumber
+          self.vehicleDescription = vehicleDescription
+          self.manufacturerName = manufacturerName
+          self.isSelfDrive = isSelfDrive
+          self.isInsured = isInsured
+          self.noOfSeat = noOfSeat
+          self.fuelType = fuelType
+        self.vehicleType = vehicleType
+        self.isWifiAvailable = isWifiAvailable
+        self.isAccessibiltyServiceAvailable = isAccessibiltyServiceAvailable
+      }
     
-    override func display()
-    {
-               super.display()
-               print("Bus Type: \(self.busType)")
-    print("Accessibility Service Avavilable: \(self.isAccessibiltyServiceAvailable)")
-               print("Wifi Available: \(self.isWifiAvailable)")
-           }*/
+    init(varDeObj: VehicleM)
+            {
+              self.vehicleIdentificationNumber = varDeObj.vehicleIdentificationNumber
+              self.vehicleDescription = varDeObj.vehicleDescription
+              self.manufacturerName = varDeObj.manufacturerName
+              self.isSelfDrive = varDeObj.isSelfDrive
+              self.isInsured = varDeObj.isInsured
+              self.noOfSeat = varDeObj.noOfSeat
+              self.fuelType = varDeObj.fuelType
+              self.vehicleType = varDeObj.vehicleType!
+                self.isAccessibiltyServiceAvailable = false
+                self.isWifiAvailable = false
+              guard let vechi = varDeObj.vehicleRent else {
+                  FinalOutput.shared.addNew(text: "")
+                  return
+                    }
+              self.details = vechi
+              
+              }
+   func display()
+   {
+    var strContent = String()
+    strContent = String(format: "Owner has a %@", "\(self.vehicleType)")
+    FinalOutput.shared.addNew(text: strContent)
+    strContent = String(format: "   - Vehicle Identification Number: %@"," \(self.vehicleIdentificationNumber)")
+    FinalOutput.shared.addNew(text: strContent)
+    strContent = String(format: "   - Vehicle Description: %@", "\(self.vehicleDescription)")
+    FinalOutput.shared.addNew(text: strContent)
+    strContent = String(format: "   - Manufacturer Name: %@","\(self.manufacturerName)")
+    FinalOutput.shared.addNew(text: strContent)
+    strContent = String(format: "   - Fule type of MotorCycle: %@", "\(self.fuelType)")
+    FinalOutput.shared.addNew(text: strContent)
+    strContent = String(format: "   - No of seats in Bus: %@","\(self.noOfSeat)")
+    FinalOutput.shared.addNew(text: strContent)
+    strContent = String(format: "   - Manufacturer: %@", "\(self.manufacturerName)")
+    guard let tes = details else {
+        FinalOutput.shared.addNew(text: "")
+        return
+    }
     }
     
-    
-
-
-   /* VehicleManagement.BusType typeOfBus;
-    boolean isAccessibilityServiceAvailable;
-    boolean isWifiAvailable;
-
-    public Bus(String vehicleIdentificationNumber, String vehicleDescription,
-               String manufacturerName, boolean isSelfDrive,
-               boolean isInsured,
-               int noOfSeat, VehicleManagement.FUEL fuelType,
-               VehicleManagement.BusType typeOfBus, boolean isAccessibilityServiceAvailable,
-               boolean isWifiAvailable) {
-        super(vehicleIdentificationNumber, vehicleDescription,
-                manufacturerName, isSelfDrive,
-                isInsured,
-                noOfSeat, fuelType);
-        super.vehicleType = VehicleManagement.VEHICLETYPE.BUS;
-        this.typeOfBus = typeOfBus;
-        this.isAccessibilityServiceAvailable = isAccessibilityServiceAvailable;
-        this.isWifiAvailable = isWifiAvailable;
-    }
-
-    public VehicleManagement.BusType getTypeOfBus() {
-        return typeOfBus;
-    }
-
-    public void setTypeOfBus(VehicleManagement.BusType typeOfBus) {
-        this.typeOfBus = typeOfBus;
-    }
-
-
-
-}*/
+}
